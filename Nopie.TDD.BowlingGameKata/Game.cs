02697a0d5167f6cdec9fc94f4302234283e04a4c
Julius Philip Bartolome <juliusbartolome@hotmail.ph>
2018-10-19 13:49:32 +0800

    added initial test for game that covers score, current frame count, and roll

--- Nopie.TDD.BowlingGameKata/Game.cs
+++ Nopie.TDD.BowlingGameKata/Game.cs
@@ -4,9 +4,22 @@ namespace Nopie.TDD.BowlingGameKata
 {
     public class Game
     {
+        private int currentFrame = 1;
+        private int score;
+
+        public int CurrentFrame => currentFrame;
+
         public int Score()
         {
-            return 0;
+            return score;
+        }
+
+        public void Roll(int pins)
+        {
+            score += pins;
+
+            if (pins == 10)
+                currentFrame += 1;
         }
     }
 }
--- Nopie.TDD.BowlingGameKataTests/GameTests.cs
+++ Nopie.TDD.BowlingGameKataTests/GameTests.cs
@@ -5,9 +5,12 @@ namespace Nopie.TDD.BowlingGameKataTests
 {
     public class GameTests
     {
+        private Game sutGame;
+
         [SetUp]
         public void Setup()
         {
+            sutGame = new Game();
         }
 
         [TearDown]
@@ -18,11 +21,57 @@ namespace Nopie.TDD.BowlingGameKataTests
         [Test]
         public void Score_WhenCalled_ShouldReturnZeroByDefault()
         {
-            var game = new Game();
-            var score = game.Score();
-            
-            Assert.That(score, Is.EqualTo(0));
+            Assert.That(sutGame.Score(), Is.EqualTo(0));
+        }
 
+        [Test]
+        [TestCase(10)]
+        [TestCase(5)]
+        [TestCase(1)]
+        [TestCase(0)]
+        public void Score_WhenCalled_AfterFirstRoll_ShouldReturnThePins(int pins)
+        {
+            sutGame.Roll(pins);
+            
+            Assert.That(sutGame.Score(), Is.EqualTo(pins));
+        }
+
+        [Test]
+        [TestCase(5, 0)]
+        [TestCase(6, 3)]
+        public void Score_WhenCalled_AfterSecondRoll_WhereTotalPinsRolledAreLessThanTen_ShouldReturnTheTotalPinsOfBothRolls(int firstRollPins, int secondRollPins)
+        {
+            var expectedScore = firstRollPins + secondRollPins;
+            sutGame.Roll(firstRollPins);
+            sutGame.Roll(secondRollPins);
+
+            Assert.That(sutGame.Score(), Is.EqualTo(expectedScore));
+        }
+
+        [Test]
+        public void CurrentFrame_WhenCalled_BeforeAnyRoll_ShouldReturnOne()
+        {
+            Assert.That(sutGame.CurrentFrame, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void CurrentFrame_WhenCalled_AfterRollingTenPins_ShouldIncrementCurrentFrameByOne()
+        {
+            var expectedCurrentFrame = sutGame.CurrentFrame + 1;
+
+            sutGame.Roll(10);
+
+            Assert.That(sutGame.CurrentFrame, Is.EqualTo(expectedCurrentFrame));
+        }
+
+        [Test]
+        public void CurrentFrame_WhenCalled_AfterRollingPinsLessThanTen_ShouldNotIncrementCurrentFrame()
+        {
+            var expectedCurrentFrame = sutGame.CurrentFrame;
+
+            sutGame.Roll(9);
+
+            Assert.That(sutGame.CurrentFrame, Is.EqualTo(expectedCurrentFrame));
         }
     }
 }
